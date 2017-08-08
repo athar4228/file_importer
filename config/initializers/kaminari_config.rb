2162000f36e1c407a9a9028c823392cb7f40e41d
Athar Noor <atharnoor2@gmail.com>
2017-08-09 01:38:48 +0500

    Person: added pagination

---
+++ config/initializers/kaminari_config.rb
@@ -0,0 +1,10 @@
+Kaminari.configure do |config|
+  config.default_per_page = 5 #TODO change this or add to model per_page if different number of items are required
+  # config.max_per_page = nil
+  # config.window = 4
+  # config.outer_window = 0
+  # config.left = 0
+  # config.right = 0
+  # config.page_method_name = :page
+  # config.param_name = :page
+end
